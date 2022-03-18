b2aa5c3f9f9fa918dc1ec562993a4ec4f16d6086
Purva <Purva@192.168.1.192>
2022-03-18 20:13:19 +0530

    Happy Holi to You

--- NewProjectGit/src/s1/Aclass.java
+++ NewProjectGit/src/s1/Aclass.java
@@ -3,7 +3,8 @@ package s1;
 public class Aclass {
 public static void main(String[] args) {
 	System.out.println("this is gitclass ");
-	
-	
+}
+public static void Newmethod() {
+	System.out.println("method added by purva on 18 march");
 }
 }
